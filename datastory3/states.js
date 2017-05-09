c33d236e5d6d72e52068ef9de7569c3b48150c27
gheffernan <tug11136@temple.edu>
2017-05-09 09:58:50 -0400

    Add files via upload

---
+++ datastory3/states.js
@@ -0,0 +1,61 @@
+
+
+	$(document).ready(function() {
+
+		$(".state-groups").click(function(){$(".state-groups.active").removeClass("active"); $(this).addClass("active"); });
+
+		$(".nationwide-btn").click(function() {$(".card").hide(); $("#national").show(); $(".state-groups.active").removeClass("active");});
+
+		$("#ME").on('click', function() {$('.card').hide();$('#ME-card').show();});
+		$("#WI").on('click', function() {$('.card').hide();$('#WI-card').show();});
+		$("#VT").on('click', function() {$('.card').hide();$('#VT-card').show();});
+		$("#NH").on('click', function() {$('.card').hide();$('#NH-card').show();});
+		$("#WA").on('click', function() {$('.card').hide();$('#WA-card').show();});
+		$("#ID").on('click', function() {$('.card').hide();$('#ID-card').show();});
+		$("#MT").on('click', function() {$('.card').hide();$('#MT-card').show();});
+		$("#ND").on('click', function() {$('.card').hide();$('#ND-card').show();});
+		$("#MN").on('click', function() {$('.card').hide();$('#MN-card').show();});
+		$("#IL").on('click', function() {$('.card').hide();$('#IL-card').show();});
+		$("#MI").on('click', function() {$('.card').hide();$('#MI-card').show();});
+		$("#NY").on('click', function() {$('.card').hide();$('#NY-card').show();});
+		$("#MA").on('click', function() {$('.card').hide();$('#MA-card').show();});
+		$("#OR").on('click', function() {$('.card').hide();$('#OR-card').show();});
+		$("#NV").on('click', function() {$('.card').hide();$('#NV-card').show();});
+		$("#WY").on('click', function() {$('.card').hide();$('#WY-card').show();});
+		$("#SD").on('click', function() {$('.card').hide();$('#SD-card').show();});
+		$("#IA").on('click', function() {$('.card').hide();$('#IA-card').show();});
+		$("#IN").on('click', function() {$('.card').hide();$('#IN-card').show();});
+		$("#OH").on('click', function() {$('.card').hide();$('#OH-card').show();});
+		$("#PA").on('click', function() {$('.card').hide();$('#PA-card').show();});
+		$("#NJ").on('click', function() {$('.card').hide();$('#NJ-card').show();});
+		$("#CT").on('click', function() {$('.card').hide();$('#CT-card').show();});
+		$("#RI").on('click', function() {$('.card').hide();$('#RI-card').show();});
+		$("#CA").on('click', function() {$('.card').hide();$('#CA-card').show();});
+		$("#UT").on('click', function() {$('.card').hide();$('#UT-card').show();});
+		$("#CO").on('click', function() {$('.card').hide();$('#CO-card').show();});
+		$("#NE").on('click', function() {$('.card').hide();$('#NE-card').show();});
+		$("#MO").on('click', function() {$('.card').hide();$('#MO-card').show();});
+		$("#KY").on('click', function() {$('.card').hide();$('#KY-card').show();});
+		$("#WV").on('click', function() {$('.card').hide();$('#WV-card').show();});
+		$("#VA").on('click', function() {$('.card').hide();$('#VA-card').show();});
+		$("#MD").on('click', function() {$('.card').hide();$('#MD-card').show();});
+		$("#DE").on('click', function() {$('.card').hide();$('#DE-card').show();});
+		$("#AZ").on('click', function() {$('.card').hide();$('#AZ-card').show();});
+		$("#NM").on('click', function() {$('.card').hide();$('#NM-card').show();});
+		$("#KS").on('click', function() {$('.card').hide();$('#KS-card').show();});
+		$("#AR").on('click', function() {$('.card').hide();$('#AR-card').show();});
+		$("#TN").on('click', function() {$('.card').hide();$('#TN-card').show();});
+		$("#NC").on('click', function() {$('.card').hide();$('#NC-card').show();});
+		$("#SC").on('click', function() {$('.card').hide();$('#SC-card').show();});
+		$("#DC").on('click', function() {$('.card').hide();$('#DC-card').show();});
+		$("#OK").on('click', function() {$('.card').hide();$('#OK-card').show();});
+		$("#LA").on('click', function() {$('.card').hide();$('#LA-card').show();});
+		$("#MS").on('click', function() {$('.card').hide();$('#MS-card').show();});
+		$("#AL").on('click', function() {$('.card').hide();$('#AL-card').show();});
+		$("#GA").on('click', function() {$('.card').hide();$('#GA-card').show();});
+		$("#AK").on('click', function() {$('.card').hide();$('#AK-card').show();});
+		$("#HI").on('click', function() {$('.card').hide();$('#HI-card').show();});
+		$("#TX").on('click', function() {$('.card').hide();$('#TX-card').show();});
+		$("#FL").on('click', function() {$('.card').hide();$('#FL-card').show();});
+
+	})
